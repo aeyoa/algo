062615192ef49b6cf8be1ae01925b8b1ad707aeb
Arseny Kogan <arsenykogan@gmail.com>
2014-03-29 23:12:47 +0000

    t02_lang: simple UI for currency converter

---
+++ lang/src/java/ru/ifmo/enf/kogan/t02_lang/Main.java
@@ -0,0 +1,10 @@
+package ru.ifmo.enf.kogan.t02_lang;
+
+/**
+ * Created by arsenykogan on 30/03/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new CurrencyConverterUI().displayUI();
+    }
+}
