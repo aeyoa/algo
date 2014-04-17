49836ea7b00e4f445de34efd4353d5ece9b26fbb
Arseny Kogan <arsenykogan@gmail.com>
2014-04-17 21:19:05 +0400

    t13 done

--- algo/src/test/ru/ifmo/enf/kogan/t13/PerceptronImplTest.java
+++ algo/src/test/ru/ifmo/enf/kogan/t13/PerceptronImplTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by arsenykogan on 12/04/14.
@@ -13,26 +12,78 @@ import java.util.List;
 public class PerceptronImplTest extends Assert {
 
     @Test
-    public void simpleTest() {
+    public void logicalOR_OnlyNeededTrainingData() {
         final PerceptronTrainingImpl perceptronTraining = new PerceptronTrainingImpl();
         final ArrayList trainingDataList = new ArrayList<PerceptronTraining.TrainingData>() {
             {
                 add(new TrainingData(Arrays.asList(1d, 0d, 1d), 1));
                 add(new TrainingData(Arrays.asList(1d, 1d, 0d), 1));
-//                add(new TrainingData(Arrays.asList(1d, 1d, 1d), 1));
-//                add(new TrainingData(Arrays.asList(1d, 0d, 0d), 0));
             }
         };
-//        System.out.println(p.getOutput(Arrays.asList(1d, 1d, 0d)));
-//        System.out.println(p.getOutput(Arrays.asList(1d, 1d, 0d)));
-//        System.out.println(p.getOutput(Arrays.asList(1d, 0d, 1d)));
-//        System.out.println(p.getOutput(Arrays.asList(1d, 1d, 1d)));
-//        System.out.println(p);
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100; i++) {
+            final PerceptronTraining.Perceptron p = perceptronTraining.train(.01, trainingDataList);
+            assertEquals(1.0, p.getOutput(Arrays.asList(1d, 1d, 0d)));
+            assertEquals(1.0, p.getOutput(Arrays.asList(1d, 0d, 1d)));
+        }
+    }
+
+    @Test
+    public void logicalOR_OnlyNeededTrainingDataSmallerStep() {
+        final PerceptronTrainingImpl perceptronTraining = new PerceptronTrainingImpl();
+        final ArrayList trainingDataList = new ArrayList<PerceptronTraining.TrainingData>() {
+            {
+                add(new TrainingData(Arrays.asList(1d, 0d, 1d), 1));
+                add(new TrainingData(Arrays.asList(1d, 1d, 0d), 1));
+            }
+        };
+
+        for (int i = 0; i < 100; i++) {
             final PerceptronTraining.Perceptron p = perceptronTraining.train(.1, trainingDataList);
             assertEquals(1.0, p.getOutput(Arrays.asList(1d, 1d, 0d)));
             assertEquals(1.0, p.getOutput(Arrays.asList(1d, 0d, 1d)));
         }
     }
+
+    @Test
+    public void logicalOR_AllTrainingData() {
+        final PerceptronTrainingImpl perceptronTraining = new PerceptronTrainingImpl();
+        final ArrayList trainingDataList = new ArrayList<PerceptronTraining.TrainingData>() {
+            {
+                add(new TrainingData(Arrays.asList(1d, 0d, 1d), 1));
+                add(new TrainingData(Arrays.asList(1d, 1d, 0d), 1));
+                add(new TrainingData(Arrays.asList(1d, 1d, 1d), 1));
+                add(new TrainingData(Arrays.asList(1d, 0d, 0d), -1));
+            }
+        };
+
+        for (int i = 0; i < 100; i++) {
+            final PerceptronTraining.Perceptron p = perceptronTraining.train(.1, trainingDataList);
+            assertEquals(1.0, p.getOutput(Arrays.asList(1d, 1d, 0d)));
+            assertEquals(1.0, p.getOutput(Arrays.asList(1d, 0d, 1d)));
+            assertEquals(1.0, p.getOutput(Arrays.asList(1d, 1d, 1d)));
+            assertEquals(-1.0, p.getOutput(Arrays.asList(1d, 0d, 0d)));
+        }
+    }
+
+        @Test
+        public void logicalAND_AllTrainingData() {
+            final PerceptronTrainingImpl perceptronTraining = new PerceptronTrainingImpl();
+            final ArrayList trainingDataList = new ArrayList<PerceptronTraining.TrainingData>() {
+                {
+                    add(new TrainingData(Arrays.asList(1d, 1d, 1d), 1));
+                    add(new TrainingData(Arrays.asList(1d, 0d, 1d), -1));
+                    add(new TrainingData(Arrays.asList(1d, 1d, 0d), -1));
+                    add(new TrainingData(Arrays.asList(1d, 0d, 0d), -1));
+                }
+            };
+
+            for (int i = 0; i < 100; i++) {
+                final PerceptronTraining.Perceptron p = perceptronTraining.train(.01, trainingDataList);
+                assertEquals(1.0, p.getOutput(Arrays.asList(1d, 1d, 1d)));
+                assertEquals(-1.0, p.getOutput(Arrays.asList(1d, 1d, 0d)));
+                assertEquals(-1.0, p.getOutput(Arrays.asList(1d, 0d, 1d)));
+                assertEquals(-1.0, p.getOutput(Arrays.asList(1d, 0d, 0d)));
+            }
+        }
 }
